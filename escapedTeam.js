d33d2ae742feb9fac92d4b88372b27224e886bfd
KituIco <kimtuico_06@rocketmail.com>
2023-03-08 16:27:01 +0800

    added teams formatting

---
+++ escapedTeam.js
@@ -0,0 +1,74 @@
+document.write('\
+<article id="team" class="wrapper style4">\
+    <div class="container">\
+        <header>\
+            <h2>We\'d like to hear from you.</h2>\
+            \
+            <div class="wrapper style5">\
+                <div class="row aln-center">\
+                    <div class="col-4 col-6-medium col-12-small">\
+                        <section class="box style3">\
+                            <span class="image profileIcon"><img src="images/logo.jpg" /></span><br>\
+                            <h3>Lonnie Gonzales</h3>\
+                            <p>Place self intro here. Here is a practical framework you can leverage to introduce yourself with confidence in any context, online or in-person: Present, past, and future.</p>\
+                        </section>\
+                    </div>\
+                    <div class="col-4 col-6-medium col-12-small">\
+                        <section class="box style3">\
+                            <span class="image profileIcon"><img src="images/logo.jpg" /></span><br>\
+                            <h3>Miguel Pardiñas</h3> \
+                            <p>Place self intro here. Here is a practical framework you can leverage to introduce yourself with confidence in any context, online or in-person: Present, past, and future.</p>\
+                        </section>\
+                    </div>\
+                    <div class="col-4 col-6-medium col-12-small">\
+                        <section class="box style3">\
+                            <span class="image profileIcon"><img src="images/tuico.png" /></span><br>\
+                            <h3>Kitu Tuico</h3>\
+                            <p>Place self intro here. Here is a practical framework you can leverage to introduce yourself with confidence in any context, online or in-person: Present, past, and future.</p>\
+                        </section>\
+                    </div>\
+                </div>\
+            </div>\
+\
+\
+        </header>\
+        <div class="row">\
+            <div class="col-12">\
+                <form action="mailto:kvtuico@up.edu.ph,another@gmail.com" method="post" enctype="text/plain">\
+                    <input style="width:16em;" type="submit" value="Contact Us"/>\
+                </form>\
+            </div>\
+            <div class="col-12">\
+                <hr />\
+                <h3>Find me on ...</h3>\
+                <ul class="social">\
+                    <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>\
+                    <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>\
+                    <li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>\
+                    <li><a href="#" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>\
+                    <li><a href="#" class="icon brands fa-tumblr"><span class="label">Tumblr</span></a></li>\
+                    <li><a href="#" class="icon brands fa-google-plus"><span class="label">Google+</span></a></li>\
+                    <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>\
+                    <!--\
+                    <li><a href="#" class="icon solid fa-rss"><span>RSS</span></a></li>\
+                    <li><a href="#" class="icon brands fa-instagram"><span>Instagram</span></a></li>\
+                    <li><a href="#" class="icon brands fa-foursquare"><span>Foursquare</span></a></li>\
+                    <li><a href="#" class="icon brands fa-skype"><span>Skype</span></a></li>\
+                    <li><a href="#" class="icon brands fa-soundcloud"><span>Soundcloud</span></a></li>\
+                    <li><a href="#" class="icon brands fa-youtube"><span>YouTube</span></a></li>\
+                    <li><a href="#" class="icon brands fa-blogger"><span>Blogger</span></a></li>\
+                    <li><a href="#" class="icon brands fa-flickr"><span>Flickr</span></a></li>\
+                    <li><a href="#" class="icon brands fa-vimeo"><span>Vimeo</span></a></li>\
+                    -->\
+                </ul>\
+                <hr />\
+            </div>\
+        </div>\
+        <footer>\
+            <ul id="copyright">\
+                <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>\
+            </ul>\
+        </footer>\
+    </div>\
+</article>\
+');
